e99e379fbdf39d276b5eb75ff73b4700d6bcbf7a
momin <faruk373@gmail.com>
2023-03-01 09:12:18 +0100

    added CRUD

--- API/db_connection.py
+++ API/db_connection.py
@@ -7,6 +7,7 @@ ca = certifi.where()
 
 
 class Database():
+
     def __init__(self) -> None:
         client = pymongo.MongoClient(
             f"mongodb+srv://{cf.mongodb['login']}:{cf.mongodb['password']}@cryptocluster0.azkufll.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
@@ -16,12 +17,37 @@ class Database():
         for collection in self.db.list_collection_names():
             print(collection)
 
-# create a function (in the Database class) to read the informations of an object.
-# create a function to delete an object.
-# create a function to modify the informations of an object.
-# create a function to add a new object.
+    def read_objects(self, collection_name, objects_ids):
+        collection = self.db[collection_name]
+        objects = collection.find({"_id": {"$in": objects_ids}})
+        return list(objects)
+
+    def update_object(self, collection_name, filters, updates):
+        collection = self.db[collection_name]
+        result = collection.update_many(filters, updates)
+        return result.modified_count
+
+    def add_object(self, collection_name, data):
+        collection = self.db[collection_name]
+        result = collection.insert_one(data)
+        return result.inserted_id
+
+    def delete_object(self, collection_name, filters):
+        collection = self.db[collection_name]
+        result = collection.delete_many(filters)
+        return result.deleted_count
 
 
 if __name__ == "__main__":
     db = Database()
     db.get_collections()
+    read_objects()
+    update_object()
+    add_object()
+    delete_object()
+
+
+# create a function to read the informations of a list of objects.
+# create a function to modify the informations of an object.
+# create a function to add a new object.
+# create a function to delete an object.

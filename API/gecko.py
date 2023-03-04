a1432fe45962ae4281309a8776319d26e67f24b1
momin <faruk373@gmail.com>
2023-03-04 13:24:55 +0100

    test

--- API/gecko.py
+++ API/gecko.py
@@ -13,12 +13,14 @@ class geckoAPI():
             f"{self.base_url}coins/markets?vs_currency={code}&order=market_cap_desc&per_page=250&page=1&sparkline=false",
             self.header)
         return response.json()
+    # recuperer premiere 250 coins 
 
     def get_history(self, id, currency):
         response = requests.get(
             f"{self.base_url}coins/{id}/market_chart?vs_currency={currency}&days=91", self.header)
         print(response.json())
         return response.json()
+    # on recupere les 91 derniers jours de l'historique de la crypto monnaie choisie dans la devise choisie
 
 
 if __name__ == "__main__":
